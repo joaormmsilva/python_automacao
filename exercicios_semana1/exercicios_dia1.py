ca55866112efc0e78294ee8ffcb979adb65edacb
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-09-30 22:40:45 -0300

    exercicios 1 semana 1

---
+++ exercicios_semana1/exercicios_dia1.py
@@ -0,0 +1,25 @@
+# revisão o basico em python 
+
+#Exercicios 1 semana 1
+
+int = 0
+float = 12.2
+bool = True
+str = 'olá'
+ 
+number = 0  #return - 0
+numberFloat = 12.2 #return - 12.2
+booleanFalse = number == numberFloat #return - False
+booleanTrue = number != numberFloat #return - True
+string = 'Hellow Word!' #return - Hellow Word
+
+print(f'O valor é {number} o tipo é {type(number)}')
+
+print(f'O valor é {numberFloat} o tipo é {type(numberFloat)}')
+
+print(f'O valor é {booleanFalse} o tipo é {type(booleanFalse)}')
+
+print(f'O valor é {booleanTrue} o tipo é {type(booleanTrue)}')
+
+print(f'O valor é {string} o tipo é {type(string)}')
+
