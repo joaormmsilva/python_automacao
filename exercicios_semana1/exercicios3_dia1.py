11206863e220a43e178340be1ef097a9b71964e6
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-09-30 23:03:50 -0300

    exercicios 3 semana 1

---
+++ exercicios_semana1/exercicios3_dia1.py
@@ -0,0 +1,21 @@
+# dia 1 semana 1 revisão o basico em python 
+
+#Exercicios 2 semana 1
+print('-'*10)
+print('Gerador de medias')
+print('-'*10)
+
+nota = []
+
+for i in range(3):
+    notas = float(input(f'digite a nota da {i+1} prova: '))
+    nota.append(notas)
+
+total = sum(nota)
+media = total / len(nota)
+
+print(f"\nA sua media final é {media:.2f}")
+if media < 6:
+    print('Voce Esta de recuperação =(')
+else:
+    print('parabens vc esta aprovado!!')
