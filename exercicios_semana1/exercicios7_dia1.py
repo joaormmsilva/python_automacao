f04b4e1c03a2adce0863299c4bcace05a419127e
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-10-01 23:35:43 -0300

    exercicios 7 semana 1

---
+++ exercicios_semana1/exercicios7_dia1.py
@@ -0,0 +1,8 @@
+# dia 1 semana 1 revisão o basico em python 
+
+#Exercicios 7 semana 1
+
+
+pessoa = {"nome": "Lucas", "idade": 28, "cidade": "Taubaté"}
+
+print(f'o nome é {pessoa["nome"]}, a idade é {pessoa["idade"]} - {pessoa["cidade"]}')
